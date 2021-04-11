ce7aee11b9514f7c827dc0b4898461ea4b4a7e4b
DESKTOP-Varun <varunkrishna0306@gmail.com>
2021-04-11 14:35:01 +0530

    Menu Screen Done

---
+++ Assets/RocketMechanics.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketMechanics : MonoBehaviour
+{
+    Rigidbody rb;
+    AudioSource audio;
+    [SerializeField]
+    float thrust;
+    [SerializeField]
+    float rotateThrust;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
+    }
+     void Update()
+    {
+        Rocketfly();
+        RocketControls();
+    }
+    private void Rocketfly()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddRelativeForce(Vector3.up * thrust);
+        }
+        if(audio.isPlaying==false)
+        {
+            audio.Play();
+        }
+        else
+        {
+            audio.Stop();
+        }
+    }
+    private void RocketControls()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward * rotateThrust * Time.deltaTime);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.forward * rotateThrust * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/SceneManage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneManage : MonoBehaviour
+{
+   public void NextScene(int sceneno)
+    {
+        SceneManager.LoadScene(sceneno);
+    }
+}
